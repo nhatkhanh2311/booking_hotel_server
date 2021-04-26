2c504f5dec98c1247f3bb886614d3f18ad1b0c2e
DESKTOP-KMNS09Q\Admin <quangtuankc040999@gmail.com>
2021-04-26 09:51:50 +0700

    - delete all

--- src/main/java/com/example/demo/entity/ERole.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_DIRECTOR,
-    ROLE_ADMIN,
-
-}
-
--- src/main/java/com/example/demo/exception/SignUpException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class SignUpException extends RuntimeException {
-    public SignUpException(String errorMessage) {
-        super(errorMessage);
-    }
-}
