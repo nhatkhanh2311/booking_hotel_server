c6443f69aa03fbe488512d0583c1d9fce2fdcb25
anhthai <hoangtrinhanhthai@gmail.com>
2021-04-26 11:00:34 +0700

    # rename

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -3,7 +3,7 @@ package com.example.demo.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "role")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
