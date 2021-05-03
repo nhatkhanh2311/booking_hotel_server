23f18a7599e5cd4a9457368bee23b9901b933488
anhthai <hoangtrinhanhthai@gmail.com>
2021-05-03 08:33:31 +0700

    + user controller

--- src/main/java/com/example/demo/entity/Room.java
+++ src/main/java/com/example/demo/entity/Room.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
@@ -29,6 +30,7 @@ public class Room {
 	private List<Date> date;
 
 	@ManyToOne
+	@JsonBackReference
 	private Hotel hotel;
 
 	@ManyToMany
