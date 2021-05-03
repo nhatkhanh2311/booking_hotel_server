3e715f8cb9c58eec846efc215936db7ec266a15e
DESKTOP-KMNS09Q\Admin <quangtuankc040999@gmail.com>
2021-05-03 08:36:24 +0700

    +commit

--- src/main/java/com/example/demo/controller/UserManageController.java
+++ src/main/java/com/example/demo/controller/UserManageController.java
@@ -1,25 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.User;
+import com.example.demo.exception.TaiKhoanNotFoundException;
 import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.security.jwt.GetUserFromToken;
 import com.example.demo.security.jwt.JwtUtils;
 import com.example.demo.service.UserService;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/user-management")
+    @RequestMapping("/user-management")
 public class UserManageController {
 @Autowired
     UserService userService;
 @Autowired
     JwtUtils jwtUtils;
+@Autowired
+    GetUserFromToken getUserFromToken;
 /*
 * API lock tai khoan
 */
@@ -46,6 +51,39 @@ public class UserManageController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Can't find this account on database", e);
         }
         return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    /*
+    * API thay doi mat khau
+    * */
+    @PreAuthorize("hasAnyRole('ADMIN', 'DIRECTOR', 'USER')")
+    @PutMapping("/authorized/user/change/{userId}")
+    public ResponseEntity<Void> thayDoiMatKhau (@PathVariable Long userId, @RequestHeader("Authorization") String token, @RequestBody JSONObject jsonObject ){
+        User user = getUserFromToken.getUserByUserNameFromJwt(token.substring(7));
+        System.out.println("Day la userName" + user.getUsername());
+        boolean checkPermission = false;
+        if(userId == user.getId()){
+            checkPermission = true;
+        }
+        if(checkPermission){
+            try{
+                BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+                String oldPass = jsonObject.getString("oldPass");
+                String newPass = jsonObject.getString("newPass");
+                if(!passwordEncoder.matches(oldPass, user.getPassword())){
+                    new ResponseEntity<Void>(HttpStatus.FORBIDDEN);
+                }
+                userService.changPassword(userId, passwordEncoder.encode(newPass));
+                    return new ResponseEntity<Void>(HttpStatus.OK);
+
+                } catch (TaiKhoanNotFoundException e) {
+                e.printStackTrace();
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Khong tim thay tai khoan trong CSDL", e);
+            }
+        }
+        else {
+            return new ResponseEntity<Void>(HttpStatus.FORBIDDEN);
+        }
     }
 
 }
