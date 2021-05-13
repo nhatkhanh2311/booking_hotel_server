2b9f01be5871af07d63614e305141a262aaeef30
anhthai <hoangtrinhanhthai@gmail.com>
2021-05-13 21:42:13 +0700

    crossOrigin

--- src/main/java/com/example/demo/controller/DirectorController.java
+++ src/main/java/com/example/demo/controller/DirectorController.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/director")
 public class DirectorController {
