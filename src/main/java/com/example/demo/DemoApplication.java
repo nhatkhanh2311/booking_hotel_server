8f2b32a93fde03f2182cb8a13bf9a814bb1cc5d4
anhthai <hoangtrinhanhthai@gmail.com>
2021-05-14 00:02:01 +0700

    -

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,22 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
+@EnableWebMvc
 public class DemoApplication {
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-                registry.addMapping("/**").allowedHeaders("*");
-                registry.addMapping("/**").allowedMethods("*");
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("Authorization");
             }
         };
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
